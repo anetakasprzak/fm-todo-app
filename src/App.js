ce78d769120a517144659fa666ad2f811e7034f3
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-05-12 17:45:56 +0100

    logo styled component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   height: auto;
-  display: grid;
-  justify-items: center;
   object-fit: cover;
   background-color: #fafafa;
   color: #494c6b;
@@ -23,10 +21,39 @@ export const ImageContainer = styled.div`
   }
 `;
 
-export const MainContent = styled.div``;
-export const Logo = styled.h1``;
+export const MainContent = styled.div`
+  margin-top: -15rem;
+  padding: 0rem 2.4rem;
+`;
+
+export const Logo = styled.h1`
+  color: #fff;
+  font-size: 2.5rem;
+  font-weight: 700;
+  letter-spacing: 1rem;
+
+  @media only screen and (min-width: 28rem) {
+    font-size: 3.2rem;
+    letter-spacing: 1.2rem;
+  }
+
+  @media only screen and (min-width: 37.75rem) {
+    font-size: 3.6rem;
+    letter-spacing: 1.3rem;
+  }
+
+  @media only screen and (min-width: 50rem) {
+    font-size: 4rem;
+    letter-spacing: 1.5rem;
+  }
+`;
+
 export const Input = styled.input``;
+
 export const List = styled.ul``;
+
 export const Item = styled.li``;
+
 export const Form = styled.form``;
+
 export const Button = styled.button``;
